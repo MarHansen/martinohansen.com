28a2f0cb70d0931bb636d454f0a3e6c89f656a2f
MarHansen <hansenmartino100@gmail.com>
2023-08-27 21:12:21 +0700

    disabling navbar scroll down to hide

--- src/components/animation/page-transition.tsx
+++ src/components/animation/page-transition.tsx
@@ -12,18 +12,18 @@ const Transition: React.FC<TransitionProps> = ({ children }) => {
       {children}
       {/* transition logic from codegrid */}
       <motion.div
-        className="slide-in fixed z-50 top-0 left-0 w-full h-screen bg-black origin-bottom"
+        className="slide-in fixed z-[500] top-0 left-0 w-full h-screen bg-black origin-bottom"
         initial={{ scaleY: 0 }}
         animate={{ scaleY: 0 }}
         exit={{ scaleY: 1 }}
-        transition={{ duration: 0.8, ease: [0.22, 1, 0.36, 1] }}
+        transition={{ duration: 0.8, delay: 0, ease: [0.22, 1, 0.36, 1] }}
       />
       <motion.div
-        className="slide-out fixed z-50 top-0 left-0 w-full h-screen bg-black origin-top-right"
+        className="slide-out fixed z-[500] top-0 left-0 w-full h-screen bg-black origin-top"
         initial={{ scaleY: 1 }}
         animate={{ scaleY: 0 }}
         exit={{ scaleY: 0 }}
-        transition={{ duration: 0.8, ease: [0.22, 1, 0.36, 1] }}
+        transition={{ duration: 0.8, delay: 0, ease: [0.22, 1, 0.36, 1] }}
       />
     </>
   );
